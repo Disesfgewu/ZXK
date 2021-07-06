47815f460614c265c7a6a9ba8d09053f837e7a38
Disesfgewu <77178740+Disesfgewu@users.noreply.github.com>
2021-07-06 09:40:47 +0800

    Update c463.cpp

--- c463.cpp
+++ c463.cpp
@@ -1,3 +1,13 @@
+// APCS 2017 / 10 第三題
+/*
+題目說明：https://apcs.csie.ntnu.edu.tw/index.php/questionstypes/previousexam/ （來源：https://zerojudge.tw/ShowProblem?problemid=c463）
+input：
+第一行有一個正整數n代表樹狀圖的節點個數，節點的編號為1到n。
+接下來有n行，第i行的第一個數字k代表節點i有k個子節點，第i行接下來的k個數字就是這些子節點的編號。
+每一行的相鄰數字間以空白隔開。
+output：
+輸出兩行各含一個整數，第一行是根節點的編號，第二行是H(T)。
+*/
 #include <bits/stdc++.h>
 using namespace std;
 const int nmax = 100001 ;
